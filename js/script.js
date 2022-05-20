e7f0662b99853c25464123696e9004ca5a0f45d0
Азат <104920129+Ankrait@users.noreply.github.com>
2022-05-20 15:59:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,27 +8,3 @@ function openMenu(e) {
    iconBurger.classList.toggle('_active');
    document.body.classList.toggle('_lock');
 }
-
-const locationItem = document.querySelectorAll('.item-location');
-const mapLocationItem = document.querySelectorAll('.location__map iframe');
-let numOfActive = 0;
-
-locationItem.forEach((element, index) => {
-   element.addEventListener('click', () => {
-      if (!element.classList.contains("_active")) {
-         element.classList.add("_active");
-         locationItem[numOfActive].classList.remove("_active");
-         mapLocationItem[numOfActive].classList.remove("_active");
-
-         for (let i = 0; i < mapLocationItem.length; i++) {
-            if (element.dataset.value == mapLocationItem[i].dataset.value) {
-               mapLocationItem[i].classList.add("_active");
-               break;
-            }
-         }
-
-         numOfActive = index;
-      }
-   });
-});
-
